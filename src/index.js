b0ac4e5f4c8bc353b9531f11b0016afbf8953c49
aseel-dweedar <archaseel.1992@gmail.com>
2022-02-10 19:35:39 +0200

    connect to redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App"
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import reducers from './reducers';
+import thunk from 'redux-thunk';
+import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"))
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.render(
+    <Provider store={store} >
+        <App />
+    </Provider>
+    , document.querySelector("#root"));
